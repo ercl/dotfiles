ce60547ecd695de39ecd12737a2d2cae8bc24de4
Eric Lee <eric50098@gmail.com>
2022-07-04 01:03:35 -0700

    Add fishshell config

---
+++ .config/fish/config.fish
@@ -0,0 +1,3 @@
+starship init fish | source
+ 
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
---
+++ .config/fish/functions/functions.fish
@@ -0,0 +1,5 @@
+function fish_user_key_bindings
+    bind \t accept-autosuggestion
+    bind \b backward-kill-word
+    bind \e\[A history-prefix-search-backward
+end
